22443828822805b5f1877c8ff1268f6ece911ad6
Criztiandev <criztiandev@gmail.com>
2025-05-04 20:02:16 +0800

    refactor: Add SOCIALS_DATA for footer links and open links in new tab

--- src/data/socials.data.ts
+++ src/data/socials.data.ts
@@ -2,21 +2,21 @@ export const SOCIALS_DATA = [
   {
     id: 1,
     name: "Twitter",
-    url: "https://twitter.com/neos",
+    url: "https://x.com/neosbotethh",
     icon: "/image/icons/x.svg",
   },
 
   {
     id: 2,
     name: "Telegram",
-    url: "https://t.me/neos",
+    url: "https://t.me/neosboteth",
     icon: "/image/icons/telegram.svg",
   },
 
   {
     id: 3,
     name: "Discord",
-    url: "https://discord.com/neos",
+    url: "/",
     icon: "/image/icons/discord.svg",
   },
 ];
