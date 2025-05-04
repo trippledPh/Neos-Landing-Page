d4dc0daca3696c08189cfc8bc3aaf1563ca83475
Criztiandev <criztiandev@gmail.com>
2025-05-04 13:44:34 +0800

    refactor: Enhance TokenDetails and TokenomicsSection components with updated font styles and layout improvements

--- src/components/molecules/token/token-details/index.tsx
+++ src/components/molecules/token/token-details/index.tsx
@@ -9,11 +9,16 @@ interface Props {
 
 const TokenDetails = ({ label, value, color, className }: Props) => {
   return (
-    <span className="flex flex-col gap-2 items-center">
+    <span className="flex flex-col gap-2 items-center font-maxima-nouva font-bold">
       <span className="text-3xl font-semibold" style={{ color }}>
         {value}
       </span>
-      <span className={cn("text-center text-lg text-white", className)}>
+      <span
+        className={cn(
+          "text-center text-lg  font-inter font-medium text-muted-foreground",
+          className
+        )}
+      >
         {label}
       </span>
     </span>
