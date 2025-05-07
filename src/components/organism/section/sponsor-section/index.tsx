aff4cd21764edcea25fd0e7769efab58c61de833
Criztiandev <criztiandev@gmail.com>
2025-05-07 20:37:05 +0800

    fix: Adjust layout in SponsorSection for better spacing and alignment

--- src/components/organism/section/sponsor-section/index.tsx
+++ src/components/organism/section/sponsor-section/index.tsx
@@ -4,7 +4,7 @@ const SponsorSection = () => {
       <span className="text-muted-foreground textl-xl">
         with technical support from:
       </span>
-      <div className="flex flex-col items-center justify-center gap-6 md:flex-row md:mt-4 lg:gap-12">
+      <div className="flex flex-col items-center justify-center gap-6 mb-12  md:flex-row md:mt-4 lg:gap-12">
         <span>
           <img
             src="/image/icons/mongodb.logo.svg"
@@ -12,7 +12,7 @@ const SponsorSection = () => {
             className="object-contain"
           />
         </span>
-        <div className="grid grid-cols-2 gap-4 lg:gap-8">
+        <div className="flex flex-col gap-4 md:grid grid-cols-2 gap-4 lg:gap-8">
           <span>
             <img src="/image/icons/heroku.logo.svg" alt="heroku" />
           </span>
