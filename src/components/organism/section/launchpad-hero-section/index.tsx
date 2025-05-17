609ae7d60f6086e29d14c66e2aa76d354f16e109
Criztiandev <criztiandev@gmail.com>
2025-05-18 03:20:16 +0800

    refactor: Update LaunchPadHeroSection and Button component styles for improved layout and consistency

--- src/components/organism/section/launchpad-hero-section/index.tsx
+++ src/components/organism/section/launchpad-hero-section/index.tsx
@@ -6,11 +6,11 @@ const LaunchPadHeroSection = () => {
   return (
     <section className="w-full flex flex-col justify-center items-center p-4 gap-6 mt-[100px] p-4  xl:flex-row xl:gap-8 xl:items-start lg:justify-start">
       <div className="flex flex-col justify-center items-center  gap-[48px] mb-[48px] md:items-center xl:items-start md:justify-center ">
-        <div className="flex flex-col items-start gap-6 md:text-center md:justify-center md:items-center">
-          <h3 className="text-[52px] leading-[48px] text-left md:text-5xl md:leading-[56px] text-wrap text-center max-w-[600px] w-fit font-maxima-nouva font-thin md:text-center md:text-[80px] ">
+        <div className="flex flex-col items-start gap-6 md:text-center md:justify-center md:items-center xl:text-left xl:justify-start xl:items-start">
+          <h3 className="text-[52px] leading-[48px] text-left md:text-5xl md:leading-[56px] text-wrap text-center max-w-[600px] w-fit font-maxima-nouva font-thin md:text-center md:text-[80px] xl:text-left xl:leading-[72px] ">
             <span className="font-bold">Neos</span> Launchpad
           </h3>
-          <p className="text-muted-foreground text-base text-left md:text-center  max-w-[600px] md:max-w-[450px] ">
+          <p className="text-muted-foreground text-base text-left md:text-center  max-w-[600px] md:max-w-[450px] xl:text-left xl:leading-[28px] xl:justify-start xl:items-start">
             Your one-stop platform for discovering, tracking, and acting on the
             hottest crypto narratives—instantly.
           </p>
