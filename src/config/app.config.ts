1e8624e2f3837bbd92fb7685e46e4a7439c31d0d
Criztiandev <criztiandev@gmail.com>
2025-05-21 19:22:07 +0800

    feat: Update package dependencies to include axios and styled-components; add type definitions for styled-components and prop-types

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,6 @@
 export const APP_CONFIG = {
   env: {
-    GITBOOK_URL: import.meta.env.VITE_GITBOOK_URL ?? "https://neos-2.gitbook.io/neos-bot",
-  }
+    GITBOOK_URL:
+      import.meta.env.VITE_GITBOOK_URL ?? "https://neos-2.gitbook.io/neos-bot",
+  },
 };
