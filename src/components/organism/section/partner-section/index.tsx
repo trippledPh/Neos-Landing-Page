c8fb8a569d114d9c026c14886a6fc52f26d32200
Criztiandev <criztiandev@gmail.com>
2025-05-21 20:30:25 +0800

    fix: Update PartnerSection styles for improved layout; increase height and margin for logo display, and replace logo assets for Aventa and Qlix

--- src/components/organism/section/partner-section/index.tsx
+++ src/components/organism/section/partner-section/index.tsx
@@ -2,7 +2,7 @@ const PartnerSection = () => {
   return (
     <section className="flex flex-col items-center justify-center mb-4 gap-4">
       <div
-        className="flex items-center gap-4 h-[250px] w-full justify-center"
+        className="flex items-center gap-4 h-[350px] w-full justify-center"
         style={{
           background: "url('/image/background/beam-background.png')",
           backgroundSize: "cover",
@@ -15,7 +15,7 @@ const PartnerSection = () => {
             Trusted by our partners
           </h2>
 
-          <div className="flex items-center gap-4 mb-4">
+          <div className="flex items-center gap-4 mb-8">
             <img src="/image/icons/aventa.logo.png" alt="aventa" />
             <img src="/image/icons/qlix.logo.png" alt="qlix" />
           </div>
